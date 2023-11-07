d971010cfc7fd82c4305eab7e6eedebfcff2738f
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2023-11-06 19:07:21 -0600

    Solución a errores en HTML y JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,32 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');   //const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.text(); //agregado-> Data declarado para linea 12 (obtengo el JSON en un Objeto String)
+  const dataOBJ = JSON.parse(data); //agregado-> data lo convierto en Objeto JSON para poder acceder a sus datos
+  console.log(data); //Para ver el Objeto String
+
+  console.log(response);//agregado ->test para ver que tiene la respuesta de la linea 9
+  console.log(dataOBJ);//agregado ->test para ver el Objeto JSON
+  console.log(dataOBJ['name'], dataOBJ["blog"], dataOBJ["location"]); //test para ver que trae
+
+  $n.textContent = dataOBJ.name;      //$n.textContent = '${dataOBJ.name}';
+  $b.textContent = dataOBJ.blog;      // $b.textContent = '${data.blog}';
+  $l.textContent = dataOBJ.location;      // $l.textContent = '${data.location}';
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`  //faltó $
 }
 
 displayUser('stolinski').catch(handleError);
+
+//referencia a Async Await: https://lenguajejs.com/javascript/asincronia/async-await/
+//referencia a textContent: https://www.javascripttutorial.net/javascript-dom/javascript-textcontent/
