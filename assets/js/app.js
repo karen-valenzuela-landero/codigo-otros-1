41a178223b2b463c31457fc5bc13cd9a46ad6f02
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2023-11-06 19:13:34 -0600

    Mejora de Solución a errores en HTML y JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,17 +7,12 @@ const $l = document.querySelector('.location');
 async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  const data = await response.text(); //agregado-> Data declarado para linea 12 (obtengo el JSON en un Objeto String)
-  const dataOBJ = JSON.parse(data); //agregado-> data lo convierto en Objeto JSON para poder acceder a sus datos
-  console.log(data); //Para ver el Objeto String
+  const data = await response.json(); //
+  console.log(data); 
 
-  console.log(response);//agregado ->test para ver que tiene la respuesta de la linea 9
-  console.log(dataOBJ);//agregado ->test para ver el Objeto JSON
-  console.log(dataOBJ['name'], dataOBJ["blog"], dataOBJ["location"]); //test para ver que trae
-
-  $n.textContent = dataOBJ.name;      //$n.textContent = '${dataOBJ.name}';
-  $b.textContent = dataOBJ.blog;      // $b.textContent = '${data.blog}';
-  $l.textContent = dataOBJ.location;      // $l.textContent = '${data.location}';
+  $n.textContent = data.name;      //$n.textContent = '${dataOBJ.name}';
+  $b.textContent = data.blog;      // $b.textContent = '${data.blog}';
+  $l.textContent = data.location;      // $l.textContent = '${data.location}';
 }
 
 function handleError(err) {
